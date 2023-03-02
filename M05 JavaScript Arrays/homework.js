e04d41814ad0f94afd658e4f7f32a556d089f7c2
ILELOPEZ <ferchu91garcia@gmail.com>
2023-03-01 21:47:01 -0300

    mi primer commit

--- M05 JavaScript Arrays/homework.js
+++ M05 JavaScript Arrays/homework.js
@@ -3,34 +3,45 @@
 function devolverPrimerElemento(array) {
    // Retornar el primer elemento del arreglo recibido por parámetro.
    // Tu código:
+   return devolverPrimerElemento=array[0];
 }
 
 function devolverUltimoElemento(array) {
    // Retornar el último elemento del arreglo recibido por parámetro.
    // Tu código:
+   return devolverUltimoElemento=array[(array.length-1)];
 }
 
 function obtenerLargoDelArray(array) {
    // Retornar la longitud del arreglo recibido por parámetro.
    // Tu código:
+   return obtenerLargoDelArray=array.length; 
 }
 
 function incrementarPorUno(array) {
    // El arreglo recibido por parámetro contiene números.
    // Retornar un arreglo con los elementos incrementados en +1.
    // Tu código:
+   var masUno= array.map((num)=>{
+      return num+1;
+   });
+   return masUno
 }
 
 function agregarItemAlFinalDelArray(array, elemento) {
    // Agrega el "elemento" al final del arreglo recibido.
    // Retorna el arreglo.
    // Tu código:
+   array.push (elemento);
+   return array;
 }
 
 function agregarItemAlComienzoDelArray(array, elemento) {
    // Agrega el "elemento" al comienzo del arreglo recibido.
    // Retorna el arreglo.
    // Tu código:
+   array.unshift(elemento);
+   return array;
 }
 
 function dePalabrasAFrase(palabras) {
@@ -39,30 +50,48 @@ function dePalabrasAFrase(palabras) {
    // con un espacio entre cada palabra.
    // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'.
    // Tu código:
+   return palabras.join (" ");
 }
 
 function arrayContiene(array, elemento) {
    // Verifica si el elemento existe dentro del arreglo recibido.
    // Retornar True si está, o False si no está.
    // Tu código:
+   var encontrar = array.find((e) => e == elemento);
+   if (encontrar == elemento){
+      return true
+   }else{
+      return false
+   }
 }
 
 function agregarNumeros(arrayOfNums) {
    // El parámetro "arrayOfNums" debe ser un arreglo de números.
    // Suma todos los elementos y retorna el resultado.
    // Tu código:
+   var resultado = arrayOfNums.reduce ((acc, el) => acc + el, 0);
+   return resultado;
 }
 
 function promedioResultadosTest(resultadosTest) {
    // El parámetro "resultadosTest" es un arreglo de números.
    // Itera (en un bucle) los elementos del arreglo y devuelve el promedio de las notas.
    // Tu código:
+   var resultado = resultadosTest.reduce ((acc, el) => acc + el, 0);
+   return resultado / resultadosTest.length
 }
 
 function numeroMasGrande(arrayOfNums) {
    // El parámetro "arrayOfNums" es un arreglo de números.
    // Retornar el número más grande.
    // Tu código:
+   var masGrande= 0
+   for(var i=0; i < arrayOfNums.length; i++){
+      if(masGrande<arrayOfNums[i]){
+      masGrande=arrayOfNums[i]
+      }
+   }
+   return masGrande
 }
 
 function multiplicarArgumentos() {
@@ -70,11 +99,26 @@ function multiplicarArgumentos() {
    // Si no se pasan argumentos retorna 0. Si se pasa un argumento, simplemente retórnalo.
    // [PISTA]: "arguments" es un arreglo.
    // Tu código:
+   var resultado = 1;
+   if (arguments.length === 0){
+      return 0
+   }
+   for (var i =0; i < arguments.length; i++){
+      resultado = resultado * arguments[i];
+   }
+   return resultado
 }
 
 function cuentoElementos(array) {
    // Desarrolla una función que retorne la cantidad de elementos del arreglo cuyo valor sea mayor que 18.
    // Tu código:
+   contador = 0;
+   for (var i = 0; i < array.length; i++){
+      if (array [i] > 18){
+         contador = contador + 1;
+      }
+   }
+   return contador
 }
 
 function diaDeLaSemana(numeroDeDia) {
@@ -82,18 +126,33 @@ function diaDeLaSemana(numeroDeDia) {
    // Realiza una función que, dado el número del día de la semana, retorne: "Es fin de semana"
    // si el día corresponde a "Sábado" o "Domingo", y "Es dia laboral" en caso contrario.
    // Tu código:
+   if (numeroDeDia === 1 || numeroDeDia === 7){
+      return "Es fin de semana"
+   }else{
+      return "Es dia laboral"
+   }
 }
 
 function empiezaConNueve(num) {
    // Esta función recibe por parámetro un número.
    // Debe retornar True si el entero inicia con 9 y False en otro caso.
    // Tu código:
+   num = num.toString ();
+   if (num[0] ==="9"){
+      return true
+   }else{
+      return false
+   }
 }
 
 function todosIguales(array) {
    // Si todos los elementos del arreglo son iguales, retornar True.
    // Caso contrario retornar False.
    // Tu código:
+   for (var i = 0; i< array.length; i++){
+      if (array [i]=== array [i + 1]) return true;
+   }
+   return false;
 }
 
 function mesesDelAño(array) {
@@ -101,18 +160,42 @@ function mesesDelAño(array) {
    // "Marzo" y "Noviembre", guardarlos en un nuevo arreglo y retornarlo.
    // Si alguno de los meses no está, retornar el string: "No se encontraron los meses pedidos".
    // Tu código:
+   var nuevoArray = [];
+   for ( var i = 0; i < array.length; i++){
+      if (array [i] === "Marzo" || array [i] ==="Noviembre" || array [i] ==="Enero"){
+         nuevoArray.push (array [i])
+      }
+   }
+   if (nuevoArray.length === 3){
+      return nuevoArray
+   }else{
+      return "No se encontraron los meses pedidos"
+   }
 }
-
 function tablaDelSeis() {
    // Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
    // La función devuelve un arreglo con los resultados de la tabla de multiplicar del 6 en orden creciente.
    // Tu código:
+   var resultadoTabla = [];
+   var multi = 0;
+   while (multi <= 10) {
+      var result = 6 * multi;
+      resultadoTabla.push (result)
+      multi ++
+   }
+   return resultadoTabla
 }
 
 function mayorACien(array) {
    // La función recibe un arreglo con enteros entre 0 y 200.
    // Recorrerlo y retornar un arreglo con todos los valores mayores a 100 (no incluye el 100).
    // Tu código:
+   var masCien = [];
+   for (var i =0; i< array.length; i++){
+      if (array [i] > 100)
+      masCien.push (array [i])
+   }
+   return masCien
 }
 
 /* ----------------------------------------------------------------------------------
